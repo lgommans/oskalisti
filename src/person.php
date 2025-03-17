lgommans/oskalisti

commit 5fbf7c53f7a7b1b27f34cbca42332d6d21fc2c37

Remove outer layer of semi-transparent boxes on wishlists (so that they are not nested anymore) and only stylise the description as >quote rather than including the title and buttons in that quote style, hopefully making it more clear what is part of one wish

src/person.php
@@ -250,23 +250,26 @@
 
 			$title_htmlescaped = htmlescape($row['title']);
 			$description_htmlescaped = htmlescape($row['description']);
-			$description_rendered = linkifiedbbcode($row['description']);
+			if (strlen($row['description']) > 0) {
+				$description_rendered = '<div class=wishinfo>' . linkifiedbbcode($row['description']) . '</div>';
+			}
+			else {
+				$description_rendered = '';
+			}
 			$html = "
 				<section id='wish$wishid_int' class='wish$personalClass$struckClass' data-id='$wishid_int' data-descriptionbb='$description_htmlescaped' data-addedbyus=$added_by_us>
-					<div class=wishinfo>
-						<div class=titleandbuttons>
-							<div class=title>$title_htmlescaped</div>
-							<button class=edit>$Edit</button>
-							<form class=inline method=POST onsubmit='return confirm(\"$R_U_sure_strike\");'>
-								<input type=hidden name=toggleStrike value='$wishid_int'>
-								<input type=submit value='$Strike'>
-							</form>
-							<button class=comment>$Comment</button>
-							$deletewishbtn
-						</div>
-						$pic
-						$description_rendered
+					<div class=titleandbuttons>
+						<div class=title>$title_htmlescaped</div>
+						<button class=edit>$Edit</button>
+						<form class=inline method=POST onsubmit='return confirm(\"$R_U_sure_strike\");'>
+							<input type=hidden name=toggleStrike value='$wishid_int'>
+							<input type=submit value='$Strike'>
+						</form>
+						<button class=comment>$Comment</button>
+						$deletewishbtn
 					</div>
+					$pic
+					$description_rendered
 					$added$edited
 					$personalText
 					$comments
@@ -275,7 +278,7 @@
 
 			if ($struck) {
 				if (strlen($struck_wishes) == 0) {
-					$struck_wishes = '<section><h2>' . tr('Struck wishes') . '</h2>';
+					$struck_wishes = '<hr><h2>' . tr('Struck wishes') . '</h2>';
 				}
 				$struck_wishes .= $html;
 			}
@@ -283,9 +286,6 @@
 				$active_wishes .= $html;
 			}
 		}
-	}
-	if (strlen($struck_wishes) > 0) {
-		$struck_wishes .= '</section>';
 	}
 	if (strlen($active_wishes) == 0) {
 		$active_wishes = tr('person has no wishes');
@@ -296,18 +296,16 @@
 	?>
 		<a href='.'>&#x25C2; <?php echo tr('Back to the overview'); ?></a><br><br>
 
+		<h2>
+			<?php echo tr('Wishlist of'); ?>
+			<span style="color: <?php echo $listownercolor_htmlescaped; ?>"><?php echo htmlescape($listownername); ?></span>
+		</h2>
+
 		<section>
-			<h2>
-				<?php echo tr('Wishlist of'); ?>
-				<span style="color: <?php echo $listownercolor_htmlescaped; ?>"><?php echo htmlescape($listownername); ?></span>
-			</h2>
-
-			<section>
-				<button id=addwish class=jumbobutton><?php echo tr('Add a wish'); ?></button>
-			</section>
-
-			<?php echo $active_wishes; ?>
+			<button id=addwish class=jumbobutton><?php echo tr('Add a wish'); ?></button>
 		</section>
+
+		<?php echo $active_wishes; ?>
 
 		<?php echo $struck_wishes; ?>
 

res/main.css
@@ -77,10 +77,12 @@ button, input[type=button], input[type=submit] {
 	box-shadow: 0px 0px 3px black;
 	border-radius: 4px;
 	margin: 1px;
-	margin-top: 3px;
+	margin-top: 5px;
 	margin-bottom: 3px;
-	padding-bottom: 2px;
-	padding-top: 2px;
+	padding-left: 8px;
+	padding-right: 8px;
+	padding-bottom: 5px;
+	padding-top: 5px;
 }
 textarea, input[type=text] {
 	background: #eee;
@@ -105,7 +107,7 @@ section {
 	padding-bottom: 20px;
 	border-radius: 20px;
 	border-color: transparent;
-	background: #ffffff1f;
+	background: #ffffff2c;
 }
 img {
 	display: none;

commit 97a77d2727b12e3cdb7751033d64f9a981921a44

Fix wish editing and commenting: 5fbf7c53 introduced a regression by changing how many parents there were between the button and the wish element with its dataset attributes. The new code is more robust, looking for the "nearest" .wish element, which sounds a bit vague but this seems to be the canonical way of finding a parent with a certain property

src/person.php
@@ -348,7 +348,7 @@
 
 			document.querySelectorAll('.wish .comment').forEach(function(btn) {
 				btn.addEventListener('click', function(ev) {
-					let wishel = btn.parentNode.parentNode.parentNode;
+					let wishel = btn.closest('.wish');
 					cmodal.querySelector('input[name=wishid]').value = wishel.dataset.id;
 					cmodal.querySelector('.title').innerText = wishel.querySelector('.title').innerText;
 					cmodal.showModal();
@@ -357,7 +357,7 @@
 
 			document.querySelectorAll('.wish .edit').forEach(function(btn) {
 				btn.addEventListener('click', function(ev) {
-					let wishel = btn.parentNode.parentNode.parentNode;
+					let wishel = btn.closest('.wish');
 					if (aoewishidfield.value == wishel.dataset.id) {
 						// they were already editing this wish: don't overwrite what they entered
 						aoemodal.showModal();